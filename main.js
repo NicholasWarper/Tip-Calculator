e9a7371679c0fbade562fbc02780624519a38fe1
NicholasWarper <abesadzenika6@gmail.com>
2025-04-06 15:05:56 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,14 @@
-const bill = document.querySelector(".bill")
-const btn1 = document.querySelector(".btn1")
-const btn2 = document.querySelector(".btn2")
-const btn3 = document.querySelector(".btn3")
-const btn4 = document.querySelector(".btn4")
-const btn5 = document.querySelector(".btn5")
-const btn6 = document.querySelector(".btn6")
-const PeopleAmount = document.querySelector(".PeopleAmount")
-const pay = document.querySelector(".pay")
-const tip = document.querySelector(".tip")
-const reset = document.querySelector(".reset")
+const more1 = document.querySelector(".more1")
+const more2 = document.querySelector(".more2")
+const more3 = document.querySelector(".more3")
+const more4 = document.querySelector(".more4")
+const A1 = document.querySelector(".A1")
+const D1 = document.querySelector(".D1")
 
-console.log(bill)
-console.log(btn1)
-console.log(btn2)
-console.log(btn3)
-console.log(btn4)
-console.log(btn5)
-console.log(btn6)
-console.log(PeopleAmount)
-console.log(pay)
-console.log(tip)
-console.log(reset)
+console.log(A1)
+console.log(more1)
 
-bill.addEventListener("input", () =>{
-    pay.textContent =  bill.value
-})
 
-reset.addEventListener("click", () =>{
-    PeopleAmount.textContent = 0
+more1.addEventListener("click", () =>{
+    D1.style.display = "block"
 })
--- style-guide.md
+++ style-guide.md
@@ -11,28 +11,20 @@ The designs were created to the following widths:
 
 ## Colors
 
-### Primary
-
-- Strong cyan: hsl(172, 67%, 45%)
-
-### Neutral
-
-- Very dark cyan: hsl(183, 100%, 15%)
-- Dark grayish cyan: hsl(186, 14%, 43%)
-- Grayish cyan: hsl(184, 14%, 56%)
-- Light grayish cyan: hsl(185, 41%, 84%)
-- Very light grayish cyan: hsl(189, 41%, 97%)
 - White: hsl(0, 0%, 100%)
+- Light pink: hsl(275, 100%, 97%)
+- Grayish purple: hsl(292, 16%, 49%)
+- Dark purple: hsl(292, 42%, 14%)
 
 ## Typography
 
 ### Body Copy
 
-- Font size (form inputs): 24px
+- Font size (paragraphs): 16px
 
 ### Font
 
-- Family: [Space Mono](https://fonts.google.com/specimen/Space+Mono)
-- Weights: 700
+- Family: [Work Sans](https://fonts.google.com/specimen/Work+Sans)
+- Weights: 400, 600, 700
 
 > 💎 [Upgrade to Pro](https://www.frontendmentor.io/pro?ref=style-guide) for design file access to see all design details and get hands-on experience using a professional workflow with tools like Figma.
